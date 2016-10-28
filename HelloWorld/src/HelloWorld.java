cc18ca9bdb808ff507179b315e7a52acc9522856
HeidlerF <dulac.93@hotmail.de>
2016-10-28 11:59:00 +0200

    added name query

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -6,6 +6,9 @@
  * @author Mohsen
  *
  */
+import java.util.Scanner;
+
+
 public class HelloWorld {
 
 	/**
@@ -13,6 +16,13 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2016 - how are you? :)");
+		System.out.println("Please enter your name: ");
+		
+		Scanner userInput = new Scanner(System.in);
+		String userName = userInput.next();
+		HelloUser user = new HelloUser(userName);		
+		user.greetUser();
+		
+		userInput.close();		
 	}
-
 }
